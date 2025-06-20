8fecf9e94c83fb6fee8c321f6b0d0b3f9ae08223
officialryder1~ <kennethvic07@gmail.com>
2025-06-20 10:36:59 +0100

    added db

--- src/lib/pocketbase.js
+++ src/lib/pocketbase.js
@@ -1,6 +1,7 @@
 import PocketBase from 'pocketbase';
+import { PUBLIC_POCKETBASE_URL } from '$env/static/public';
 
-export const pb = new PocketBase('http://127.0.0.1:8090');
+export const pb = new PocketBase(PUBLIC_POCKETBASE_URL);
 
 export function getFileUrl(collectionId, recordId, fileName){
     return `http://127.0.0.1:8090/api/files/${collectionId}/${recordId}/${fileName}`;
